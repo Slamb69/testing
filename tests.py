9532f4ffca1e1885010af44a073a9ed07b6f9dcb
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-20 18:05:18 -0700

    Further study complete except Improving the Mel Check

--- tests.py
+++ tests.py
@@ -21,9 +21,9 @@ class PartyTests(unittest.TestCase):
 
     def test_no_rsvp_yet(self):
         """Do users who haven't RSVPed see the correct view?"""
-
         # FIXME: Add a test to show we haven't RSVP'd yet
-        print "FIXME"
+        result = self.client.get("/")
+        self.assertIn("Please RSVP", str(result.data))
 
     def test_rsvp(self):
         """Do RSVPed users see the correct view?"""
@@ -32,16 +32,19 @@ class PartyTests(unittest.TestCase):
 
         result = self.client.post("/rsvp", data=rsvp_info,
                                   follow_redirects=True)
+# FIXME: check that once we log in we see party details--but not the form!
+        result = self.client.get("/")
+        self.assertIn("Party Details", str(result.data))
 
-        # FIXME: check that once we log in we see party details--but not the form!
-        print "FIXME"
+        self.assertNotIn("Please RSVP", str(result.data))
 
     def test_rsvp_mel(self):
         """Can we keep Mel out?"""
+        rsvp_info = {'name': "Mel Melitpolski", 'email': "mel@ubermelon.com"}
 
-        # FIXME: write a test that mel can't invite himself
-        pass
-        print "FIXME"
+        result = self.client.post("/rsvp", data=rsvp_info,
+                                  follow_redirects=True)
+
 
 
 if __name__ == "__main__":
